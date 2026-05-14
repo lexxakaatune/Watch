3e1a7640b1d7e4f149e3e0da7152128e34a05ab7
LexTheDev <alexcasimia@gmail.com>
2026-05-14 07:45:23 +0100

    main debug

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -3,6 +3,8 @@ import ReactDOM from 'react-dom/client';
 import App from './App';
 import './css/index.css';
 
+<div> Main jsx page div 1  </div>
+
 const savedTheme = localStorage.getItem('theme');
 if (savedTheme) {
   document.documentElement.setAttribute('data-theme', savedTheme);
@@ -11,9 +13,11 @@ if (savedTheme) {
   document.documentElement.setAttribute('data-theme', prefersDark ? 'dark' : 'light');
 }
 
+<div> Main jsx page div 2 </div>
+
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <div> Main jsx page </div>
+    <div> Main jsx page div 3 </div>
     <App />
   </React.StrictMode>
 );
