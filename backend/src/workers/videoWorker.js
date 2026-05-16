54c0150c3bde92637931084e7e3034865e74be87
LexTheDev <alexcasimia@gmail.com>
2026-05-16 06:43:05 +0100

    update video worker

--- backend/src/workers/videoWorker.js
+++ backend/src/workers/videoWorker.js
@@ -6,9 +6,7 @@ const { s3, bucket } = require('../config/s3');
 
 const connectDB = async () => {
   const uri = process.env.MONGO_URI;
-  if (!uri) {
-    throw new Error('MONGO_URI is not defined in environment variables');
-  }
+  if (!uri) throw new Error('MONGO_URI not defined');
   await mongoose.connect(uri);
   console.log('MongoDB Atlas connected');
 };
@@ -22,19 +20,20 @@ const startWorker = async () => {
     const { videoId, key } = job.data;
     try {
       await Video.findByIdAndUpdate(videoId, { processingProgress: 10 });
-      await new Promise(resolve => setTimeout(resolve, 2000));
+      await new Promise(r => setTimeout(r, 2000));
       await Video.findByIdAndUpdate(videoId, { processingProgress: 40 });
-      await new Promise(resolve => setTimeout(resolve, 2000));
+      await new Promise(r => setTimeout(r, 2000));
       await Video.findByIdAndUpdate(videoId, { processingProgress: 70 });
-      await new Promise(resolve => setTimeout(resolve, 2000));
+      await new Promise(r => setTimeout(r, 2000));
 
       const baseName = key.split('/').pop().replace(/\.[^/.]+$/, '');
-      const hlsUrl = `https://${bucket}.s3.${process.env.AWS_REGION}.amazonaws.com/hls/${baseName}/master.m3u8`;
+      const b2Base = `${process.env.B2_ENDPOINT}/${bucket}`;
+      const hlsUrl = `${b2Base}/hls/${baseName}/master.m3u8`;
       const resolutions = [
-        { resolution: '240p', url: `https://${bucket}.s3.${process.env.AWS_REGION}.amazonaws.com/hls/${baseName}/240p.m3u8` },
-        { resolution: '480p', url: `https://${bucket}.s3.${process.env.AWS_REGION}.amazonaws.com/hls/${baseName}/480p.m3u8` },
-        { resolution: '720p', url: `https://${bucket}.s3.${process.env.AWS_REGION}.amazonaws.com/hls/${baseName}/720p.m3u8` },
-        { resolution: '1080p', url: `https://${bucket}.s3.${process.env.AWS_REGION}.amazonaws.com/hls/${baseName}/1080p.m3u8` }
+        { resolution: '240p', url: `${b2Base}/hls/${baseName}/240p.m3u8` },
+        { resolution: '480p', url: `${b2Base}/hls/${baseName}/480p.m3u8` },
+        { resolution: '720p', url: `${b2Base}/hls/${baseName}/720p.m3u8` },
+        { resolution: '1080p', url: `${b2Base}/hls/${baseName}/1080p.m3u8` }
       ];
 
       await Video.findByIdAndUpdate(videoId, {
@@ -59,10 +58,11 @@ const startWorker = async () => {
     console.error(`Video job ${job.id} failed:`, err.message);
   });
 
-  console.log('Video worker started and listening for jobs...');
+  console.log('Video worker started...');
 };
 
 startWorker().catch(err => {
-  console.error('Worker failed to start:', err.message);
+  console.error('Worker failed:', err.message);
   process.exit(1);
 });
+
